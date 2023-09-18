f65150e4215be6f1b40505291dfeeab81d33c54b
TomasBodas <tomasbodas20@gmail.com>
2023-09-19 08:15:16 +0900

    fix: dateRange fix for the last date.

--- ChartJsExample/wwwroot/scripts/chartjs_app.js
+++ ChartJsExample/wwwroot/scripts/chartjs_app.js
@@ -69,7 +69,6 @@ function createDataSet(reports, startDate, endDate) {
 
     reports.forEach((r) => {
         const date = new Date(r["workDate"]);
-        console.log(date);
         const maintenance = r["workTime"]["maintenance"];
         const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
         if (dataSet[formattedDate] !== undefined) {
